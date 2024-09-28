9334fb891d29a64ed3178a252a81dab5871f268f
iisador <i.isador.max89@gmail.com>
2024-09-28 15:33:51 +0700

    sedfs

--- build.bat
+++ build.bat
@@ -3,8 +3,16 @@
 @REM call docker build -f Dockerfile.native -t seabattle .
 @REM call docker save seabattle -o seabattle.tar
 @REM timeout /t 1
-call scp .\target\seabattle-0.0.1-runner isador@192.168.1.58:~/seabattle/seabattle-0.0.1-runner
+@REM call scp .\target\seabattle-0.0.1-runner isador@192.168.1.58:~/seabattle/seabattle-0.0.1-runner
 @REM call scp .\db\init-scripts\1_schema.sql isador@192.168.1.58:~/seabattle/init-scripts/1_schema.sql
 @REM call scp .\db\init-scripts\2_data.sql isador@192.168.1.58:~/seabattle/init-scripts/2_data.sql
-call ssh isador@192.168.1.58 "sudo docker build -f ~/seabattle/Dockerfile.native -t seabattle ~/seabattle"
+@REM call ssh isador@192.168.1.58 "sudo docker build -f ~/seabattle/Dockerfile.native -t seabattle ~/seabattle"
 @REM call ssh isador@192.168.1.58 "sudo docker load -i ~/seabattle/seabattle.tar"
+
+call mvn clean package
+call ssh isador@192.168.1.58 "sudo docker compose -f /home/isador/seabattle/compose.yaml stop"
+call ssh isador@192.168.1.58 "rm -rf /home/isador/seabattle/app"
+call scp -r .\target\quarkus-app isador@192.168.1.58:~/seabattle/app
+call ssh isador@192.168.1.58 "sudo docker image rm -f seabattle-seabattle"
+call ssh isador@192.168.1.58 "sudo docker compose -f /home/isador/seabattle/compose.yaml build --no-cache"
+call ssh isador@192.168.1.58 "sudo docker compose -f /home/isador/seabattle/compose.yaml up -d"
