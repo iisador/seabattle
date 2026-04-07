a7c14bf5b2f70db64e0ea2b3cc14b2693c11f832
iisador <i.isador.max89@gmail.com>
2026-04-07 23:02:14 +0700

    добавлены звуки, изменены шрифты. Удалены лого

--- src/main/resources/META-INF/resources/js/aud.js
+++ src/main/resources/META-INF/resources/js/aud.js
@@ -5,7 +5,7 @@ class Sounds {
     #finishes = [];
     #fails = [];
     #starts = [];
-
+    #custom = [];
     #nowPlaying;
 
     constructor() {
@@ -26,6 +26,14 @@ class Sounds {
         this.#fails.push(new Audio('/audio/fail2.ogg'));
 
         this.#starts.push(new Audio('/audio/start.ogg'));
+
+        this.#custom.push(new Audio("/audio/it's_getting_boring.mp3"));
+        this.#custom.push(new Audio("/audio/where's_the_map_Billy.mp3"));
+        this.#custom.push(new Audio("/audio/you're_firmly_aground.mp3"));
+        this.#custom.push(new Audio("/audio/this_is_our_last_chance.mp3"));
+        this.#custom.push(new Audio("/audio/i_vote_to_kill.mp3"));
+        this.#custom.push(new Audio("/audio/i_don't_understand_sir.mp3"));
+        this.#custom.push(new Audio("/audio/i_don't_like_anything_at_all.mp3"));
     }
 
     playRandom(arr) {
@@ -57,6 +65,34 @@ class Sounds {
 
     playRandomFail() {
         this.playRandom(this.#fails);
+    }
+
+    playItsGettingBoring() {
+        this.playRandom([this.#custom[0]]);
+    }
+
+    playWhereIsTheMap() {
+        this.playRandom([this.#custom[1]]);
+    }
+
+    playYouReFirmlyAground() {
+        this.playRandom([this.#custom[2]]);
+    }
+
+    playThisIsOurLastChance() {
+        this.playRandom([this.#custom[3]]);
+    }
+
+    playIVoteToKill() {
+        this.playRandom([this.#custom[4]]);
+    }
+
+    playIDontUnderstandSir() {
+        this.playRandom([this.#custom[5]]);
+    }
+
+    playIDontLikeAnythingAtAll() {
+        this.playRandom([this.#custom[6]]);
     }
 
     playRandomStart() {
